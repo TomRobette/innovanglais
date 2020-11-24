bff94221f5a88375cf290dbfb308dfc5c96c5dff
Hugo-FONTAINE1 <h.fontaine5962@gmail.com>
2020-11-24 15:10:08 +0000

    0.0.4

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Categorie;
+use App\Form\AjoutCategorieType;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategorieController extends AbstractController
+{
+    /**
+     * @Route("/ajoutCategorie", name="ajoutCategorie")
+     */
+    public function ajoutCategorie(Request $request)
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm(AjoutCategorieType::class,$categorie);
+
+
+
+        if ($request->isMethod('POST')){            
+            $form -> handleRequest ($request);            
+            if($form->isValid()){              
+              $em = $this->getDoctrine()->getManager();              
+              $em->persist($categorie);              
+              $em->flush();        
+            $this->addFlash('notice','>Catégorie ajouté'); 
+           
+            } 
+            return $this->redirectToRoute('ajoutCategorie');
+          }
+
+        return $this->render('categorie/ajoutCategorie.html.twig', [
+            'form'=>$form->createView()
+        ]);
+    }
+}
