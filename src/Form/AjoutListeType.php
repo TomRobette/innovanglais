7a080965d0e1abca334278199510253a77e62265
TomRobette <robettetom@gmail.com>
2020-12-15 15:16:52 +0000

    0.2.16

--- src/Form/AjoutListeType.php
+++ src/Form/AjoutListeType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use App\Form\AjoutVocabulaireType;
 use App\Entity\Vocabulaire;
 
 class AjoutListeType extends AbstractType
@@ -19,12 +20,13 @@ class AjoutListeType extends AbstractType
     {
         $builder
             ->add('nom',TextType::class)
-            ->add('listeMots', CollectionType::class, array(
-                'entry_type' => new Vocabulaire(),
-                'entry_options' => [
-                    'attr' => ['class' => 'email-box'],
-                ]
-            ))
+            ->add('listeMots', EntityType::class, [
+                'class'     => Vocabulaire::class,
+                'choice_label' => 'mot',
+                'label'     => 'Quel sera le mot?',
+                'expanded'  => true,
+                'multiple'  => true,
+            ])
             ->add('idEntreprise',EntityType::Class,['class' => 'App\Entity\Entreprise','choice_label' => 'libelle', 'required'=> false])
             ->add('theme',EntityType::Class,array('class' => 'App\Entity\Theme','choice_label' => 'libelle'))
             ->add('ajouter', SubmitType::class)
