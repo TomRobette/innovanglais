65de03d43be7532c9a193caa7e569fea6a95ded7
Remi-ACCART <remi.accart.sin@gmail.com>
2020-12-15 10:36:05 +0000

    0.2.13

--- migrations/Version20201215103342.php
+++ migrations/Version20201215103342.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201215095300 extends AbstractMigration
+final class Version20201215103342 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20201215095300 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE liste ADD nom VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE abonnement CHANGE libele libelle VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE liste DROP nom');
+        $this->addSql('ALTER TABLE abonnement CHANGE libelle libele VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
